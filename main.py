66680534479e1bdb37030f3f630e5a87533a7c51
ObradovicSrdjan <obradovicsrd@gmail.com>
2025-02-13 15:00:13 +1100

    Added comparator

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from Bio import SeqIO
 from cds_finder import get_coding_sequences
 from protein_parser import parse_protein_fasta
 from protein_translator import translate_multiple_dna_sequences
+from protein_comparator import compare_proteins, print_comparison_results
+
 
 DEFAULT_ECOLI_WHOLE_GENOME_FASTA_FILE = "data/ecoli/genome/GCF_000005845.2_ASM584v2_genomic.fna"
 DEFAULT_ECOLI_PROTEIN_FASTA_FILE = "data/ecoli/proteins/uniprotkb_Escherichia_coli_str_K_12_sub_2025_02_12.fasta"
@@ -47,6 +49,12 @@ def main():
     print(f"Found {len(protein_sequences)} protein sequences.")
     for idx, protein in enumerate(protein_sequences[:5]):  # Preview first 5 sequences
         print(f"Protein {idx + 1}: {protein[:30]}...")
+
+
+    # Compare translated proteins with those from the FASTA file
+    matches = compare_proteins(proteins, protein_sequences)
+    print_comparison_results(matches)
+
 
 if __name__ == "__main__":
     main()
